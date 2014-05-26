851b579a133a2a11be91160914b44741dc27a6ee
Alexandre <alexandre.bacco@gmail.com>
2014-05-26 18:51:32 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+A very lightweight yet powerful PHP state machine
+=================================================
+
+Define your states, define your transitions and your callbacks: we do the rest.
+The era of hard-coded states is over!
+
+[![Build Status](https://travis-ci.org/winzou/state-machine.svg?branch=master)](https://travis-ci.org/winzou/state-machine)
+
+Installation
+---------------
+
+### Installation (via composer)
+```js
+{
+      "require": {
+        "winzou/state-machine": "dev-master"
+    }
+}
+```
---
+++ src/SM/Event/TransitionEvent.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * This file is part of the StateMachine package.
+ *
+ * (c) Alexandre Bacco
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SM\Event;
+
+use SM\StateMachine\StateMachineInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+class TransitionEvent extends Event
+{
+    /**
+     * @var string
+     */
+    protected $transition;
+
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * @var StateMachineInterface
+     */
+    protected $stateMachine;
+
+    /**
+     * @var bool
+     */
+    protected $rejected = false;
+
+    /**
+     * @param string                $transition   Name of the transition being applied
+     * @param array                 $config       Configuration of the transition
+     * @param StateMachineInterface $stateMachine State machine
+     */
+    public function __construct($transition, array $config, StateMachineInterface $stateMachine)
+    {
+        $this->transition   = $transition;
+        $this->config       = $config;
+        $this->stateMachine = $stateMachine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTransition()
+    {
+        return $this->transition;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * @return StateMachineInterface
+     */
+    public function getStateMachine()
+    {
+        return $this->stateMachine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->stateMachine->getState();
+    }
+
+    /**
+     * @param bool $reject
+     */
+    public function setRejected($reject = true)
+    {
+        $this->rejected = (bool) $reject;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRejected()
+    {
+        return $this->rejected;
+    }
+}
